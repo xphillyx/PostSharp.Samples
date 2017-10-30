b797263a88c43b11d3f537de93f4519d3b74e15b
Gael Fraiteur <gael@postsharp.net>
2017-10-30 17:19:42 +0100

    Cleaning namespace imports.

--- Diagnostics/PostSharp.Samples.Logging.Serilog/Program.cs
+++ Diagnostics/PostSharp.Samples.Logging.Serilog/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using PostSharp.Patterns.Diagnostics;
+﻿using PostSharp.Patterns.Diagnostics;
 using PostSharp.Patterns.Diagnostics.Backends.Serilog;
 using PostSharp.Samples.Logging.BusinessLogic;
 using Serilog;
