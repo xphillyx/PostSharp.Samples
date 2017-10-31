4c279e44233401654383422e4bb75853f0f4ac11
Gael Fraiteur <gael@postsharp.net>
2017-10-31 09:05:36 +0100

    Adding sample PostSharp.Samples.Logging.Loupe.

---
+++ Diagnostics/PostSharp.Samples.Logging.Loupe/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gibraltar.Agent;
+using PostSharp.Patterns.Diagnostics;
+using PostSharp.Patterns.Diagnostics.Backends.Loupe;
+using PostSharp.Samples.Logging.BusinessLogic;
+
+[assembly: Log]
+
+namespace PostSharp.Samples.Logging.Loupe
+{
+    [Log(AttributeExclude = true)]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gibraltar.Agent.Log.StartSession();
+            Gibraltar.Agent.Log.ShowLiveViewer();
+
+            // Configure PostSharp Logging to use Loupe.
+            LoggingServices.DefaultBackend = new LoupeLoggingBackend();
+
+            // Simulate some business logic.
+            QueueProcessor.ProcessQueue(@".\Private$\SyncRequestQueue");
+
+            
+
+            
+            Console.WriteLine("Press Enter to finish.");
+            Console.ReadLine();
+
+            Gibraltar.Agent.Log.EndSession();
+        }
+    }
+}
