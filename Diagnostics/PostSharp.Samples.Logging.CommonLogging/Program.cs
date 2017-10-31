808864abff3db0b86f5d4b085d502f095f26f92f
Gael Fraiteur <gael@postsharp.net>
2017-10-31 09:38:28 +0100

    Adding project PostSharp.Samples.Logging.CommonLogging.

---
+++ Diagnostics/PostSharp.Samples.Logging.CommonLogging/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using Common.Logging.Simple;
+using PostSharp.Patterns.Diagnostics;
+using PostSharp.Patterns.Diagnostics.Backends.CommonLogging;
+using PostSharp.Samples.Logging.BusinessLogic;
+
+[assembly: Log]
+
+namespace PostSharp.Samples.Logging.CommonLogging
+{
+    [Log(AttributeExclude = true)]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Configure Common.Logging to direct outputs to the system console.
+            LogManager.Adapter = new ConsoleOutLoggerFactoryAdapter();
+
+            // Configure PostSharp Logging to direct outputs to Common.Logging.
+            LoggingServices.DefaultBackend = new CommonLoggingLoggingBackend();
+
+            // Simulate some business logic.
+            QueueProcessor.ProcessQueue(@".\Private$\SyncRequestQueue");
+        }
+    }
+}
---
+++ Diagnostics/PostSharp.Samples.Logging.CommonLogging/README.md
@@ -0,0 +1,6 @@
+# PostSharp.Samples.Logging.Console
+
+This example demonstrates how to configure the PostSharp Logging aspect so that it prints its output to Common.Logging.
+Here Common.Logging is configured to direct outputs to the system console.
+
+
