5405414902e6f2272b7ac455473664fbcf9d5a2c
Gael Fraiteur <gael@postsharp.net>
2018-01-17 16:33:45 +0100

    Serilog: indentation.

--- Diagnostics/PostSharp.Samples.Logging.Serilog/Program.cs
+++ Diagnostics/PostSharp.Samples.Logging.Serilog/Program.cs
@@ -13,11 +13,14 @@ namespace PostSharp.Samples.Logging.Serilog
   {
     private static void Main(string[] args)
     {
-      // Configure Serilog.
+      // The output template must include {Indent} for nice output.
+      const string template = "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level}] {Indent:l}{Message}{NewLine}{Exception}";
+
+      // Configure a Serilog logger.
       var logger = new LoggerConfiguration()
         .MinimumLevel.Debug()
-        .WriteTo.File("serilog.log")
-        .WriteTo.ColoredConsole()
+        .WriteTo.File("serilog.log", outputTemplate: template)
+        .WriteTo.ColoredConsole(outputTemplate: template)
         .CreateLogger();
 
       // Configure PostSharp Logging to use Serilog
